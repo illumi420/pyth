797e5295245f4940a05126b15f7933c1b8f2ce3a
illumi420 <illumi.420@icloud.com>
2021-02-18 14:26:18 +0100

    small changes

--- geometrie.py
+++ geometrie.py
@@ -1,6 +1,25 @@
 import math
 
-print("Von welchem geometriischen Körper wollen Sie das Volumen berechnen")
+volumen = 0
+
+
+def kreiskegelVolumen(radius, hoehe):
+    return (1/3) * math.pi * math.pow(radius, 2) * hoehe
+
+
+def kreiszylinderVolumen(radius, hoehe):
+    return math.pi * math.pow(radius, 2) * hoehe
+
+
+def quaderVolumen(breite, laenge, hoehe):
+    return breite * laenge * hoehe
+
+
+def wuerfelVolumen(seitenlaenge):
+    return seitenlaenge**3
+
+
+print("Von welchem geometrieschen Körper wollen Sie das Volumen berechnen")
 print("(1) Kreiskegel")
 print("(2) Kreiszylinder")
 print("(3) Quader")
@@ -10,33 +29,42 @@ print("(q) Programm verlassen")
 list = ("1", "2", "3", "4", "q")
 for element in list:
     num = input("Ihre Wahl: ").lower()
-
+# kreiskegel
     if num == list[0]:
-        r = float(input("Bitte Radius eingeben: "))
-        # if  r >= 0:
-        #   continue
-        # else:
-        #print("fehlerhafte Eingabe! bitte versuch nochmal")
-
-        h = float(input("Bitte Höhe eingeben: "))
-        print(f"Volumen:", (1/3) * math.pi * math.pow(r, 2) * h, "cm\u00b3")
-
+        try:
+            r = float(input("Bitte Radius eingeben: "))
+            h = float(input("Bitte Höhe eingeben: "))
+            volumen = kreiskegelVolumen(r, h)
+        except:
+            print("Fehlerhafte Eingabe von Radius oder Hoehe")
+# kreiszylinder
     elif num == list[1]:
-        r = float(input("Bitte Radius eingeben: "))
-        h = float(input("Bitte Höhe eingeben: "))
-        print("Volumen:", math.pi * math.pow(r, 2) * h, "cm\u00b3")
-
+        try:
+            r = float(input("Bitte Radius eingeben: "))
+            h = float(input("Bitte Höhe eingeben: "))
+            volumen = kreiszylinderVolumen(r, h)
+        except:
+            print("Fehlerhafte Eingabe von Radius oder Hoehe")
+# Quader
     elif num == list[2]:
-        a = float(input("Bitte Breite eingeben: "))
-        b = float(input("Bitte Länge eingeben: "))
-        c = float(input("bitte Höhe eingeben: "))
-        print("Volumen:", a*b*c, "cm\u00b3")
-
+        try:
+            a = float(input("Bitte Breite eingeben: "))
+            b = float(input("Bitte Länge eingeben: "))
+            c = float(input("bitte Höhe eingeben: "))
+            volumen = quaderVolumen(a, b, c)
+        except:
+            print("Fehlerhafte Eingabe von Breite, Länge oder Hoehe")
+# Würfel
     elif num == list[3]:
-        a = float(input("Bitte Seitenlänge eingeben: "))
-        print("Volumen:", a**3, "cm\u00b3")
+        try:
+            a = float(input("Bitte Seitenlänge eingeben: "))
+            volumen = wuerfelVolumen(a)
+        except:
+            print("Fehlerhafte Eingabe von Seitenlänge")
 
     elif num == list[4]:
         exit()
     else:
         print("Sie habe eine fehlerhafte Auswahl getroffen.")
+    if volumen != 0:
+        print(f"Volume :", volumen, "cm\u00b3")
--- lottozahlengenerator.py
+++ lottozahlengenerator.py
@@ -1,42 +1,39 @@
 import random as r
+lotto_zahlen = []
 
 # To make sure that no random number is duplicated in the list
 
 
 def pruefeZahl(liste, zahl):
-    if len(liste) == 0:
-        return 0
-    for i in range(len(liste)):
-        if liste[i] == zahl:
-            return 1
-    return 0
-# To sort the list (Badzura recommendation)
+    if zahl not in liste:
+        return False
 
 
-"""
-def sortierListe(liste):
-    n = len(liste)
-    i = 0
-    j = 1
-    while i < n-1:
-        while j < n:
-            if liste[i-1] > liste[j]:
-                temp = liste[j-1]
-                liste[j-1] = liste[j]
-                liste[j] = temp
-            return liste
-"""
+def lottoZahlen(liste):
+    counter = 0
+    while counter < 6:
+        random_zahl = r.randint(1, 49)
+        if pruefeZahl(liste, random_zahl) is False:
+            liste.append(random_zahl)
+            counter = counter + 1
+        else:
+            random_zahl = r.randint(1, 49)
+    return liste
 
-lotto_zahlen = []
-counter = 0
-while counter < 6:
-    random_zahl = r.randint(1, 49)
-    pruef = pruefeZahl(lotto_zahlen, random_zahl)
-    if pruef == 0:
-        lotto_zahlen.append(random_zahl)
-        sorted_liste = sortierListe(lotto_zahlen)
-        counter = counter + 1
-    lotto_zahlen = sorted(lotto_zahlen, reverse=False)
-    print(f"Zahl {counter}:", random_zahl)
-print(lotto_zahlen)
-print(sorted_liste)
+# To sort the list
+
+
+def sortiereListe(liste):
+    for i in range(len(liste)):  # defines the indexes
+        for j in range(len(liste)-1):  # j will be the value saved in the indexes
+            if liste[j] > liste[j+1]:  # if first bigger than second
+                # exchanges the position
+                liste[j], liste[j+1] = liste[j+1], liste[j]
+    return liste
+
+
+lottoZahlen(lotto_zahlen)
+sortiereListe(lotto_zahlen)
+for random_zahl in lotto_zahlen:
+    index = lotto_zahlen.index(random_zahl)
+    print(f'Zahl {index +1}: {random_zahl}')
