ecf67b606164064c5dd004bfc831fc764891c895
illumi420 <illumi.420@icloud.com>
2021-02-18 16:46:47 +0100

    tripleen

---
+++ Triple_num.py
@@ -0,0 +1,12 @@
+var = 0
+def gleichZahl(zahl):
+    erste_digit = (zahl // 100)
+    last_2_digits = zahl % 100
+    if last_2_digits == 11:
+        zahl = 111 * erste_digit
+        return zahl
+
+for i in reversed(range(111,1000,100)):
+    if i % 2 != 0:
+        var = var + 100
+        print(gleichZahl(i))
