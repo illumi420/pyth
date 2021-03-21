dd89abefc636576dd9f8800d4f1d35099c2564f2
illumi420 <illumi.420@icloud.com>
2021-03-21 12:51:53 +0100

    optimizing

--- School_stuff/questionnaire.py
+++ School_stuff/questionnaire.py
@@ -1,5 +1,10 @@
-questions = ["Is the project broken down into work packages and Tasks? ", "Is the structure defined? ",
-             "Has The work packages and the tasks been assigned in the project structure plan? ", "Has the tasks and work packages been coded? ", "Has the completeness been checked? "]
+questions = [
+    "Is the project broken down into work packages and Tasks? ",
+    "Is the structure defined? ",
+    "Has The work packages and the tasks been assigned in the project structure plan? ",
+    "Has the tasks and work packages been coded? ",
+    "Has the completeness been checked? "
+]
 
 question_answer = {}
 
@@ -8,6 +13,7 @@ def questionnaireFunc(q_list):
     print("Answer the questions with yes or no.")
     for i in q_list:
         answer = input(i).lower()
+        #assaining list item as key and item input as value
         question_answer[i] = answer
 
 
@@ -16,13 +22,17 @@ questionnaireFunc(questions)
 
 def checkAnswers(q_a_list):
     if any("no" in value for value in q_a_list.items()):
-        print("For the following questions, work steps are apparently still pending in the planning:")
+        print(
+            "For the following questions, work steps are apparently still pending in the planning:"
+        )
         for key, value in q_a_list.items():
             if "no" == value:
                 print(key)
 
     elif all("yes" in value for value in q_a_list.items()):
-        print("You have done a good job and can continue with the timing as planned.")
+        print(
+            "You have done a good job and can continue with the timing as planned."
+        )
 
 
 checkAnswers(question_answer)
