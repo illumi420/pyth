0dba79b8966daa332d10f54e25b36c424c2b0ea8
illumi420 <illumi.420@icloud.com>
2021-03-22 23:51:23 +0100

    ring-bahn station_duration halfworks

--- projekt/ring-bahn.py
+++ projekt/ring-bahn.py
@@ -8,14 +8,21 @@ stations = [
     "Südkreuz"
 ]
 
-km_long = 37, 0
-minuets_long = 59
 minuets_till_next_station = [
     2, 1, 2, 2, 2, 2, 1, 2, 1, 2, 3, 3, 2, 3, 2, 3, 2, 2, 2, 6, 2, 3, 2, 1, 4,
     2
 ]
+minuets_long = sum(minuets_till_next_station)  #59
+km_long = 37, 0
 
-print(sum(minuets_till_next_station))
+station_duration = {}
+
+for i in stations:
+    for j in minuets_till_next_station:
+        station_duration[i] = j
+
+print(station_duration)
+# print(sum(minuets_till_next_station))
 
 # r_41 = print("r41:", stations)
 # print()
